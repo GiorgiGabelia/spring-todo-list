3cc1da4e01e8185f5de43966dc56dd7eed50670a
GiorgiGabelia <giorgi.gabelia321@gmail.com>
2024-08-30 13:38:00 +0400

    feat: add h2 and jpa to dependencies, update security filterChain

--- src/main/java/com/myGroup/myFirstWebApp/security/SpringSecurityConfig.java
+++ src/main/java/com/myGroup/myFirstWebApp/security/SpringSecurityConfig.java
@@ -2,11 +2,14 @@ package com.myGroup.myFirstWebApp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -38,5 +41,17 @@ public class SpringSecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // These 2 lines repeat what is already there by default; When overriding default security filters everything should be defined again;
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions(Customizer.withDefaults()).disable();
+
+        return http.build();
     }
 }
